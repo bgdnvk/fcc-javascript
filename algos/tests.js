0d345b3a5f18cafa3f71780356e9cc8f3a1556d1
bgdnvk <bnovykov@gmail.com>
2020-07-11 15:40:36 +0200

    14

--- algos/tests.js
+++ algos/tests.js
@@ -1,5 +1,34 @@
 function smallestCommons(arr) {
-  return arr;
+
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+
+  // console.log(min);
+  // console.log(max);
+
+  // let range = [];
+  // for(let i = min; i<max; i++ ){
+  //   range.push(i);
+  // }
+
+  // let found = false;
+  // while(!found){
+
+  // }
+
+  // 
+  let sol = max;
+
+  for (let i = max - 1; i >= min; i--) {
+    // Each time the solution checks (i.e. sol%i===0) it won't be necessary
+    // to increment 'max' to our solution and restart the loop
+    if (sol % i) {
+      sol += max;
+      i = max;
+    }
+  }
+  return sol;
+
 }
 
 
