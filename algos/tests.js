75714d71efe78ea8b36c9f97763c09cb549aa348
bgdnvk <bnovykov@gmail.com>
2020-07-13 07:31:35 +0200

    first prj

--- algos/tests.js
+++ algos/tests.js
@@ -1,22 +1,38 @@
-function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447;
+function palindrome(str) {
+  // 
+  const regex = /[a-zA-Z0-9]/;
+  
+  let letters_arr = [...str].filter(e => regex.test(e))
 
-  //Looping through each key in arr object
-  for (var prop in arr) {
-    //Rounding off the orbital period value
-    var orbitalPer = Math.round(
-      2 * Math.PI * Math.sqrt(Math.pow(arr[prop].avgAlt + earthRadius, 3) / GM)
-    );
-    //deleting the avgAlt property
-    delete arr[prop].avgAlt;
-    //adding orbitalPeriod property
-    arr[prop].orbitalPeriod = orbitalPer;
-  }
+  let reversed = [...letters_arr].reverse();
+  
+  let n1 = letters_arr.join().toLowerCase();
+  let n2 = reversed.join().toLowerCase();
+  
+  // console.log("reversed is "+reversed);
+  // console.log(n1);
+  // console.log("normal is: "+letters_arr);
+  // console.log(n2);
 
-  return arr;
+  return n1 === n2? true: false;
+
+  // return JSON.stringify(letters_arr) === JSON.stringify(reversed)?
+  // true: false;
 }
 
+
+// console.log(
+// palindrome("eye")
+// );
 console.log(
-  orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }])
+  palindrome("A man, a plan, a canal. Panama")
+);
+// console.log(
+//   palindrome("My age is 0, 0 si ega ym.")
+// );
+// console.log(
+//   palindrome("not a palindrome")
+// );
+console.log(
+  palindrome("_eye")
 );
--- projects/palindrome checker.js
+++ projects/palindrome checker.js
@@ -1,8 +1,39 @@
 function palindrome(str) {
-    return true;
-  }
+  // 
+  const regex = /[a-zA-Z0-9]/;
   
+  let letters_arr = [...str].filter(e => regex.test(e))
+
+  let reversed = [...letters_arr].reverse();
   
+  let n1 = letters_arr.join().toLowerCase();
+  let n2 = reversed.join().toLowerCase();
   
-  palindrome("eye");
+  // console.log("reversed is "+reversed);
+  // console.log(n1);
+  // console.log("normal is: "+letters_arr);
+  // console.log(n2);
+
+  return n1 === n2? true: false;
+
+  // return JSON.stringify(letters_arr) === JSON.stringify(reversed)?
+  // true: false;
+}
+
+
+// console.log(
+// palindrome("eye")
+// );
+console.log(
+  palindrome("A man, a plan, a canal. Panama")
+);
+// console.log(
+//   palindrome("My age is 0, 0 si ega ym.")
+// );
+// console.log(
+//   palindrome("not a palindrome")
+// );
+console.log(
+  palindrome("_eye")
+);
   
