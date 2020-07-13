b9f4ce04c1b621461f0ffe856702e9399286eec0
bgdnvk <bnovykov@gmail.com>
2020-07-13 11:15:43 +0200

    tel

--- algos/tests.js
+++ algos/tests.js
@@ -1,5 +1,8 @@
 function telephoneCheck(str) {
-  return true;
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
 }
 
-telephoneCheck("555-555-5555");
+console.log(
+  telephoneCheck("555-555-5555")
+);
